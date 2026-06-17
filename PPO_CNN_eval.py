8f96b689456081cac8ede5ac157970bee269dca4
Frankenmos <vladpop1997@gmail.com>
2026-06-17 09:34:41 +0300

    Remove legacy PPO_CNN fossils

--- PPO_CNN_eval.py
+++
@@ -1,8 +0,0 @@
-"""Backward-compatible launcher shim for the renamed evaluation module."""
-
-from eval import *  # noqa: F401,F403
-from eval import app, main
-
-
-if __name__ == "__main__":
-    app.run(main)
